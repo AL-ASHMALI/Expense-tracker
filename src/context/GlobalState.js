a25a80fcabaf966df3c964048f360942abfbd6cc
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-08-05 22:48:31 +0100

    adding the delete functionality to delete the history

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,25 +1,41 @@
-import React, { createContext, useReducer} from 'react'; 
-import AppReducer from './AppReducer';
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
-// creating the initial state 
+// creating the initial state
 const initialState = {
-    transactions: [
-        { id: 1, text: 'T-shirt', amount: -20},
-        { id: 1, text: 'Salary', amount: 400},
-        { id: 1, text: 'Football', amount: -10},
-        { id: 1, text: 'Book', amount: -20},
-        { id: 1, text: 'Eat out', amount: -40}
-    ]
-}
+  transactions: [
+    { id: 1, text: "T-shirt", amount: -20 },
+    { id: 2, text: "Salary", amount: 400 },
+    { id: 3, text: "Football", amount: -10 },
+    { id: 4, text: "Book", amount: -20 },
+    { id: 5, text: "Eat out", amount: -40 },
+  ],
+};
 
 //create global context using createContext
-export const GlobalContext = createContext(initialState); 
+export const GlobalContext = createContext(initialState);
 
-//provider component 
+//provider component
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState); 
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (<GlobalContext.Provider value = {{transactions: state.transactions}}> {/* this allows to access the transactions in the initialState*/} 
-        {children}
-    </GlobalContext.Provider>)
-}
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+      }}
+    >
+      {/* this allows to access the transactions in the initialState*/}
+      {children}
+    </GlobalContext.Provider>
+  );
+};
