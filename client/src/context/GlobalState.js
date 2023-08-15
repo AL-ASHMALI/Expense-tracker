720ca37450a85169a63244513e4f9c4973d9bbf5
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2023-08-16 00:34:34 +0100

    started to connect server and client

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,9 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // creating the initial state
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true,
 };
 //create global context using createContext
 export const GlobalContext = createContext(initialState);
@@ -13,6 +16,22 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
+  async function getTransaction() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data, // this will get all the transactions from the database
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error, // this will get the error message
+      });
+    }
+  }
+
   function deleteTransaction(id) {
     dispatch({
       type: "DELETE_TRANSACTION",
