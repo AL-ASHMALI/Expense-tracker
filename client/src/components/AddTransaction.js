9f87093f46ff70acc3b35472c76cb0041a7b1b2d
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2024-01-27 23:25:10 +0000

    code refactoring and setting up workflow

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from "react";
-import { GlobalContext } from "../context/GlobalState";
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const AddTransaction = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
   const { addTransaction } = useContext(GlobalContext);
@@ -16,34 +16,36 @@ const AddTransaction = () => {
     };
 
     addTransaction(newTransaction);
+    setText(' ');
+    setAmount(' ');
   };
 
   return (
     <>
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
           <input
-            type="text"
+            type='text'
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
+            placeholder='Enter text...'
           />
         </div>
-        <div className="form-control">
-          <label htmlFor="amount">
+        <div className='form-control'>
+          <label htmlFor='amount'>
             Amount <br />
             (negative - for expense, positive + for income)
           </label>
           <input
-            type="number"
+            type='number'
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
+            placeholder='Enter amount...'
           />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className='btn'>Add transaction</button>
       </form>
     </>
   );
